97b2c78fb0113f0a8e92ad5ff6130f790a801b15
Temich93s <nil>
2021-10-14 13:43:14 +1000

    Пройдена тема: Конфликт доступа к сквозным параметрам

--- Memory Safety/main.swift
+++ Memory Safety/main.swift
@@ -18,3 +18,38 @@ var one = 1
 
 // Доступ к памяти с правами чтения, где хранится данная переменная
 print("We're number \(one)!")
+
+
+//MARK: Конфликт доступа к сквозным параметрам
+print("\n//Конфликт доступа к сквозным параметрам")
+
+var stepSize = 1
+
+func increment(_ number: inout Int) {
+    number += stepSize
+}
+
+//increment(&stepSize)
+// Error: conflicting accesses to stepSize
+
+// Создадим явную копию
+var copyOfStepSize = stepSize
+increment(&copyOfStepSize)
+ 
+// Обновим оригинал
+stepSize = copyOfStepSize
+print(stepSize)
+// stepSize равен 2
+
+func balance(_ x: inout Int, _ y: inout Int) {
+    let sum = x + y
+    x = sum / 2
+    y = sum - x
+}
+
+var playerOneScore = 42
+var playerTwoScore = 30
+balance(&playerOneScore, &playerTwoScore)  // OK
+print(playerOneScore, playerTwoScore)
+//balance(&playerOneScore, &playerOneScore)
+// Ошибка: Conflicting accesses to playerOneScore
