2a9702872f73be978ff773a3f42b93a53f9a5d22
Temich93s <nil>
2021-10-14 13:53:26 +1000

    Пройдена тема: Конфликт доступа к self в методах

--- Memory Safety/main.swift
+++ Memory Safety/main.swift
@@ -53,3 +53,32 @@ balance(&playerOneScore, &playerTwoScore)  // OK
 print(playerOneScore, playerTwoScore)
 //balance(&playerOneScore, &playerOneScore)
 // Ошибка: Conflicting accesses to playerOneScore
+
+
+//MARK: Конфликт доступа к self в методах
+print("\n//Конфликт доступа к self в методах")
+
+struct Player {
+    var name: String
+    var health: Int
+    var energy: Int
+    
+    static let maxHealth = 10
+    mutating func restoreHealth() {
+        health = Player.maxHealth
+    }
+}
+
+extension Player {
+    mutating func shareHealth(with teammate: inout Player) {
+        balance(&teammate.health, &health)
+    }
+}
+ 
+var oscar = Player(name: "Oscar", health: 10, energy: 10)
+var maria = Player(name: "Maria", health: 5, energy: 10)
+oscar.shareHealth(with: &maria)  // OK
+print(oscar.health, maria.health)
+
+//oscar.shareHealth(with: &oscar)
+// Ошибка: conflicting accesses to oscar
